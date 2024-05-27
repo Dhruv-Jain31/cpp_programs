ea3b16fb7c0529fd641ecec5406a3cbe4c7b71ef
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-05-28 00:07:30 +0530

    basic cpp program

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    vector<int> arr;
+    cout<<"Hello World" << " I am Dhruv" <<endl;
+    cout<<"I am learning cpp";
+    return 0;
+}
