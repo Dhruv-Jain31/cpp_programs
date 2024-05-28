973b0b0b5e1c64802f981591557439847fe68284
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-05-29 00:05:46 +0530

    added lines to prevent buffer flushing

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -36,6 +36,10 @@ vector<string> fizzbuzz(int n){
 
 int main(){
 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     vector<string> output = fizzbuzz(30);
     for(string x : output){
         cout<< x <<",";
