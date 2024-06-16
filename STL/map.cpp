8b5e9d94199bbef91b52624076ba9e0d99a14a44
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-17 00:06:34 +0530

    map.cpp

--- STL/map.cpp
+++ STL/map.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 #include <map>
+#include <list>
 #include <string>
 
 using namespace std;
 
+int main(){
+
+    map<string, list<string>> phonebook;
+
+    phonebook["Dhruv"].push_back("+917037446563");
+    phonebook["Dhruv"].push_back("+925683945543");
+    phonebook["Dhruv"].push_back("+911731080304");
+
+    phonebook["Anurag"].push_back("+45382848282");
+    phonebook["Anurag"].push_back("+874292848291");
+
+    // print function
+
+     for(pair<string, list<string>> item : phonebook){
+        cout << item.first << " - ";
+        for (string number : item.second) {  //item.second is a list<string> representing the phone numbers 
+            cout << number << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
