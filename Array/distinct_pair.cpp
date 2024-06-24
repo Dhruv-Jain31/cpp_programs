9eeab0fc619c5882fb81ba48236269b58557ad6c
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-25 00:49:56 +0530

    modified the function to achieve all the distinct elements using set STL

--- Array/distinct_pair.cpp
+++ Array/distinct_pair.cpp
@@ -4,45 +4,61 @@ whose sum is equal to K.*/
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <set>
 
 using namespace std;
 
-vector<int> pairSum(vector<int> arr,int target_sum){ // return type is a vector int
+vector<pair<int,int>> pairSum(vector<int> arr,int target_sum){ // return type is a vector int
     // logic
     unordered_set<int> s;
-    vector<int> result;
+    //vector<pair<int,int>> result;
+
+    set<pair<int,int>> result_set;
 
     for(int i=0; i<arr.size(); i++){
 
         int x = target_sum - arr[i];
         if(s.find(x) != s.end()){ // element is found in set
+        /*if only one distinct element to be found
             result.push_back(x);
             result.push_back(arr[i]);
-            return result;
+            return result;*/
+
+            // to get all distinct pairs
+            result_set.insert({x,arr[i]});
         }
 
-        // inserting the current number inside set. 
+        // inserting the current number inside set.
         // imp. to be defined after the if condition to avoid duplicacy even if it is not in the array.
         s.insert(arr[i]);
     }
-    return {}; // we return empty vector if nothing is present
+
+    //return {}; // we return empty vector if nothing is present in case of only one distinct element
+
+     //converting set to vector
+    vector<pair<int, int>> result(result_set.begin(), result_set.end());
+    return result;
+
 
 }
 
 int main(){
 
-    vector<int> arr{10, 5, 2, 3, -6, 9, 11, 4, 2, 0};
+    vector<int> arr{10, 5, 2, 3, -6, 9, 11, 4, 0};
     cout<< "Enter target Sum: ";
     int sum;
     cin >> sum;
     cout << endl;
 
-    auto p = pairSum(arr,sum);
-    if(p.size() == 0){
+    auto pairs = pairSum(arr,sum);
+    if(pairs.size() == 0){
         cout << "No such pair" << endl;
     }
     else{
-        cout << "pair is: " << p[0] << "," << p[1] << endl;
+        cout << "pair is: "<< endl;
+        for(auto p: pairs){
+            cout << p.first << "," << p.second << endl;
+        }
     }
 
     return 0;
