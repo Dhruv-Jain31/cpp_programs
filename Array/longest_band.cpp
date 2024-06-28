af19c9a4da7ef998df974c2827d65f52c0bd99bf
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-28 15:07:49 +0530

    added logic to print longest band

--- Array/longest_band.cpp
+++ Array/longest_band.cpp
@@ -12,6 +12,7 @@ using namespace std;
 int longest_band(vector<int>arr){
     int n = arr.size();
     unordered_set<int> s;
+    vector<int> longest_band;
     int largest_len = 1;
 
     // data inside set
@@ -28,8 +29,11 @@ int longest_band(vector<int>arr){
             // we start the chain from that band
             int next_no = element + 1;
             int cnt = 1;
+            vector<int> current_band;
+            current_band.push_back(parent);
 
             while(s.find(next_no) != s.end()){
+                current_band.push_back(next_no);
                 next_no++;
                 cnt++;
             }
@@ -37,8 +41,18 @@ int longest_band(vector<int>arr){
             if (cnt > largest_len){
                 largest_len = cnt;
             }
+
+            if(current_band.size() > longest_band.size()){
+                longest_band = current_band;
+            }
         }
     }
+
+    for(int num : longest_band){
+            cout << num << ",";
+        }
+
+        cout << endl;
 
     return largest_len;
 
@@ -48,7 +62,7 @@ int longest_band(vector<int>arr){
 
 int main(){
 
-    vector<int> arr{1, 9, 3, 0, 18, 5, 2, 4, 10, 7, 12, 6};
+    vector<int> arr{1, 9, 3, 0, 18, 5, 2, 4, 10, 7, 12, 6, 8};
     cout << longest_band(arr);
 
     return 0;
