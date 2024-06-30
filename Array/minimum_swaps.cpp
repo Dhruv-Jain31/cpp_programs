dbeca8aeeb1d43575ebe0aaf583c02a7c446c590
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-30 21:12:44 +0530

    added comment to minimum_swaps.cpp

---
+++ Array/minimum_swaps.cpp
@@ -0,0 +1,12 @@
+/*Given an array of size N, find the minimum number of swaps needed to make array sorted
+
+sample Input
+a1 = [5,4,3,2,1]
+
+sample output = 2
+
+sample Input
+a2 = [10,11,5,4,3,2,1]
+
+sample Output = 4 */
+
