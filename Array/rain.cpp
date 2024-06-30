145a1b39d6918a36c2a7aa47fe853ea44f87b0ea
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-30 15:52:23 +0530

    done the rain water code

--- Array/rain.cpp
+++ Array/rain.cpp
@@ -15,7 +15,7 @@ In this case, 6 units of rain water (blue section) are being trapped..*/
 
 using namespace std;
 
-int trapped_water(vector<int> heights){
+int trapped_water(vector<int> height){
 
     // if there are two bars, then no water can be trapped
     int n = height.size();
@@ -24,11 +24,24 @@ int trapped_water(vector<int> heights){
     }
 
     // Left MAX, Right MAX
+    vector<int> left(n,0) , right(n,0);
+    left[0] = height[0];
+    right[n-1] = height[n-1];
+
+    for(int i = 1; i<n; i++){
+        left[i] = max(left[i-1],height[i]); // iterating from left to right and storing the max element
+        right[n-i-1] = max(right[n-i],height[n-i-1]); // iterating from right to left and storing the max
+    }
+
+    //water level
+    int water = 0;
+    for(int i = 0; i<n; i++){
+        water += min(left[i],right[i]) - height[i];
+    }
+
+    return water;
 }
 
-
-
-  
 
 int main(){
     vector<int> water = {0,1,0,2,1,0,1,3,2,1,2,1};
