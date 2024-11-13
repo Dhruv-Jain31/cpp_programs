10d58b0b544655bfde9bfe7a6cbc17ec97017517
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-11-13 16:33:39 +0530

    added generic function in linked list

--- Linked_list/basic.cpp
+++ Linked_list/basic.cpp
@@ -1,43 +1,54 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <string>
 
-class node{
+using namespace std; // Allows using 'string' without 'std::'
+
+template<typename T>
+class Node {
 public:
-     int data;
-     node* next;
+    T data;
+    Node* next;
 
-     node(int data){
+    Node(T data) {
         this->data = data;
         next = NULL;
-     }
+    }
 };
+
 // ----------Linked List ----------------------//
-void insertAtHead(node * &head, int data){ // here we pass the reference of head to maintain the uniqueness
-    if(head==NULL){
-        head = new node(data);
+template<typename T>
+void insertAtHead(Node<T>* &head, T data) {
+    if (head == NULL) {
+        head = new Node<T>(data);
         return;
     }
-    //otherwise
-    node * n = new node(data);
+    Node<T>* n = new Node<T>(data);
     n->next = head;
     head = n;
 }
 
-void printLL(node * head){  //here copy of head is made which points to the node
-    while(head!=NULL){
-        cout << head->data <<"-->";
+template<typename T>
+void printLL(Node<T>* head) {
+    while (head != NULL) {
+        cout << head->data << "-->";
         head = head->next;
     }
-    cout <<endl;
+    cout << "NULL" << endl;
 }
 
-int main(){
-    node* head = NULL;
-    insertAtHead(head,4);
-    insertAtHead(head,3);
-    insertAtHead(head,2);
-    insertAtHead(head,1);
+int main() {
+    Node<int>* head = NULL;  // Integer linked list
+    insertAtHead(head, 4);
+    insertAtHead(head, 3);
+    insertAtHead(head, 2);
+    insertAtHead(head, 1);
     printLL(head);
+
+    Node<string>* strHead = NULL;  // String linked list, now we can use string literals directly
+    insertAtHead(strHead, std::string("apple"));// std::string is added to convert const char* to string
+    insertAtHead(strHead, std::string("banana"));//in c++ both are considered different
+    insertAtHead(strHead, std::string("cherry"));
+    printLL(strHead);
 
     return 0;
 }
