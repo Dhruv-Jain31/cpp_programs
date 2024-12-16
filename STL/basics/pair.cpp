4d99c826784c92fe78a61186ae410392dc5be3e2
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-12-16 14:52:32 +0530

    added more functions for pairs

--- STL/basics/pair.cpp
+++ STL/basics/pair.cpp
@@ -11,11 +11,16 @@ int main(){
     p2.second = 30;
 
     pair<string,pair<int,int>>p3;
+    pair<pair<string,int>,int>p4;
+
     p3.first = "Dhruv";
     p3.second.first = 21;
     p3.second.second = 77;
 
+    p4 = make_pair(make_pair("Dhruv",31),31);
+
     cout << p1.first << " " << p1.second << endl;
     cout << p2.first << " " << p2.second << endl;
     cout << p3.first <<" " << p3.second.first <<" "<< p3.second.second << endl;
+    cout << p4.first.first << " " << p4.first.second <<" "<< p4.second << endl;
 }
